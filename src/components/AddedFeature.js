e9a7351d4e5591fb9032c0f7767b66cc07aedbb5
Justin Lohner <lohner4@gmail.com>
2020-05-12 13:32:37 -0600

    Added connect function to added features and added feature components

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const AddedFeature = props => {
-  return (
-    <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
-    </li>
-  );
+	return (
+		<li>
+			{/* Add an onClick to run a function to remove a feature */}
+			<button className="button">X</button>
+			{props.feature.name}
+		</li>
+	);
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+	return {
+		name: state.features.car.name
+	};
+};
+
+export default connect(mapStateToProps, {})(AddedFeature);
